2f2b2c8db799d6c1fc68fa7cab5f8d2d2a1151e3
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2021-04-15 20:49:30 -0300

    update database config

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => $url,
+//            'url' => $url,
             'host' => $host,
             'port' => env('DB_PORT', '3306'),
             'database' => $database,
