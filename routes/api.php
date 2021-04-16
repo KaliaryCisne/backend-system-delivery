53e4ca757b4e780083404e57c9205852a92ee6cd
Kaliary Cisne de Sousa da Silva <kaliarycisne@gmail.com>
2021-04-15 21:36:38 -0300

    route home

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::prefix('v1')->namespace('Api\V1')->group(function () {
     Route::prefix('delivery')->namespace('Delivery')->group(function () {
         Route::post('create', 'CreateController@create')->name('create');
